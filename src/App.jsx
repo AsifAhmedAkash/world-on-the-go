3b4f4e285c8f5abbadf504818d7caccafe40d350
Asif Ahmed <asifahmedakash32@gmail.com>
2026-03-17 08:30:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Countries from './components/Countries/Countries'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+
+        <div>
+          <h1>React World on the go...</h1>
+          <Countries></Countries>
+        </div>
+
+
+      </section>
+
+      <div className="ticks"></div>
+
+
+
+    </>
+  )
+}
+
+export default App
